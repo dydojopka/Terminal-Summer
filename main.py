0ac285cfc9ee1cdf0aeb5735340ac052bb3ea61e
dydojopka <ashroev.ev@gmail.com>
2025-07-10 12:14:39 +0800

    Добавлено меню паузы

--- main.py
+++ main.py
@@ -6,12 +6,29 @@ from textual.reactive import reactive
 from textual.widgets import Button, Label, Footer, Header, Static
 from textual.widget import Widget
 
-class GameUI(Static):
+class PauseMenuContainer(VerticalScroll):
+    """Доп контейнер для меню паузы(Для отопражения title)"""
+    BORDER_TITLE="Пауза"
+
+    def compose(self):
+        yield Button("Продолжить", id="btn-continue")
+        yield Button("Сохранить", id="btn-save")
+        yield Button("Загрузить", id="btn-load")
+        yield Button("Настройки", id="btn-settings")
+        yield Button("В главное меню", id="btn-menu")
+    
+class PauseMenu(Static):
+    """Виджет-контейнер для меню паузы"""
+    def compose(self):
+        yield PauseMenuContainer()
+
+
+class NovelMenu(Static):
     """Виджет-контейнер для текст бара и кнопок"""
     def compose(self):
-        yield Button("Назад", id="back")
+        yield Button("Назад", id="btn-back")
         yield TextBar(id="text-bar")
-        yield Button("Вперёд", id="next")
+        yield Button("Вперёд", id="btn-next")
 
 class TextBar(Widget):
     """Виджет текст бара"""
@@ -31,22 +48,23 @@ class TerminalSummer(App):
 
     BINDINGS = [
         #("space", "next_scene", "Далее"),
+        ("escape", "pause_game", "Пауза"),
         ("b", "prev_scene", "Назад"),
         ("n", "next_scene", "Вперёд"),
     ]
 
     def compose(self) -> ComposeResult:
-        #yield Header(show_clock=True)
+        yield Header(show_clock=True, classes="hidden")
         yield Footer()
         with Vertical(id="novel-mode"):
             yield Static("", id="bg-cg", classes="ascii-art")
-            yield GameUI()
+            yield NovelMenu(id="novel-menu")
+            yield PauseMenu(id="pause-menu", classes="hidden")
+            #yield SettingsMenu(id="settings-menu", classes="hidden")
+            #yield MainMenu(id="main-menu", classes="hidden")
 
-    def on_mount(self) -> None:
-        """Действия при запуске приложения"""
-        self.preload_scenes()  # Предзагрузка всех сцен
-        self.load_scene(self.current_scene)
 
+    # ============ Функции - прочие ============
     def preload_scenes(self):
         """Предзагрузка всех сцен в кэш"""
         try:
@@ -70,15 +88,35 @@ class TerminalSummer(App):
         self.current_scene = scene_name
         self.sub_title = f"Scene: {scene_name}"
 
+    def get_scene_list(self) -> list:
+        """Получение списка доступных сцен"""
+        try:
+            files = [f[:-4] for f in os.listdir(self.scenes_dir) 
+                     if f.endswith(".txt")]
+            return sorted(files)
+        except FileNotFoundError:
+            return []
+
+
+    # ============ Функции - on_ ============
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Обработка событий при нажатии кнопки"""
         button_id = event.button.id
 
-        if button_id == "next": # Кнопка "Вперёд"
+        if button_id == "btn-next": # Кнопка "Вперёд"
             self.action_next_scene()
-        elif button_id == "back": # Кнопка "Назад"
+        elif button_id == "btn-back": # Кнопка "Назад"
             self.action_prev_scene()
+        elif button_id == "btn-continue": # Кнопка "Продолжить"
+            self.action_pause_game()
 
+    def on_mount(self) -> None:
+        """Действия при запуске приложения"""
+        self.preload_scenes()  # Предзагрузка всех сцен
+        self.load_scene(self.current_scene)
+
+
+    # ============ Функции - бинды ============
     def action_prev_scene(self) -> None:
         """Переключение на предыдущую сцену"""
         scenes = self.get_scene_list()
@@ -95,18 +133,35 @@ class TerminalSummer(App):
             new_index = min(len(scenes) - 1, current_index + 1)
             self.load_scene(scenes[new_index])
 
-    def get_scene_list(self) -> list:
-        """Получение списка доступных сцен"""
-        try:
-            files = [f[:-4] for f in os.listdir(self.scenes_dir) 
-                     if f.endswith(".txt")]
-            return sorted(files)
-        except FileNotFoundError:
-            return []
+    def action_pause_game(self) -> None:
+        """Открытие меню паузы"""
+        pause_menu = self.query_one("#pause-menu")
+        novel_menu = self.query_one("#novel-menu")
+        bg_cg = self.query_one("#bg-cg")
+        #settings_menu = self.query_one("#setting-menu")
+        #main_menu = self.query_one("#main-menu")
 
-    def action_toggle_dark(self) -> None:
-        """Смена тёмного/светлого режима"""
-        self.theme = "textual-dark" if self.theme == "textual-light" else "textual-light"
+        # Переключение видимости элементов
+        if pause_menu.has_class("hidden"):
+            # Включаем паузу: скрытие диологового окна, кнопок перемотки и задника
+            novel_menu.add_class("hidden")
+            bg_cg.add_class("hidden")
+
+            # Показ меню паузы
+            pause_menu.remove_class("hidden")
+        else:
+            # Выключаем паузу: скрытие меню паузы
+            pause_menu.add_class("hidden")
+
+            # Показ диологового окна, кнопок перемотки и задника
+            novel_menu.remove_class("hidden")
+            bg_cg.remove_class("hidden")
+
+
+
+    # def action_toggle_dark(self) -> None:
+    #     """Смена тёмного/светлого режима"""
+    #     self.theme = "textual-dark" if self.theme == "textual-light" else "textual-light"
 
 
 if __name__ == "__main__":
